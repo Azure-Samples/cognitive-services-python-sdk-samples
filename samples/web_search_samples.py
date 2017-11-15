dcb59cc9ca44b5d32a08dfdc1be4cecaa24368b1
Laurent Mazuel <laurent.mazuel@gmail.com>
2017-11-14 16:57:43 -0800

    Add basic websearch sample

---
+++ samples/web_search_samples.py
@@ -0,0 +1,61 @@
+from azure.cognitiveservices.search.websearch import WebSearchAPI
+from msrest.authentication import CognitiveServicesCredentials
+
+def result_types_lookup(subscription_key):
+    """WebSearchResultTypesLookup.
+
+    This will look up a single query (Xbox) and print out name and url for first web, image, news and videos results.
+    """
+    client = WebSearchAPI(CognitiveServicesCredentials(subscription_key))
+
+    web_data = client.web.search(query="xbox")
+    print("Searched for Query# \" Xbox \"")
+
+    # WebPages
+    if web_data.web_pages.value:
+
+        print("Webpage Results#{}".format(len(web_data.web_pages.value)))
+
+        first_web_page = web_data.web_pages.value[0]
+        print("First web page name: {} ".format(first_web_page.name))
+        print("First web page URL: {} ".format(first_web_page.url))
+
+    else:
+        print("Didn't see any Web data..")
+
+    # Images
+    if web_data.images.value:
+
+        print("Image Results#{}".format(len(web_data.images.value)))
+
+        first_image = web_data.images.value[0]
+        print("First Image name: {} ".format(first_image.name))
+        print("First Image URL: {} ".format(first_image.url))
+
+    else:
+        print("Didn't see any Image..")
+    
+    # News
+    if web_data.news.value:
+
+        print("News Results#{}".format(len(web_data.news.value)))
+
+        first_news = web_data.news.value[0]
+        print("First News name: {} ".format(first_news.name))
+        print("First News URL: {} ".format(first_news.url))
+
+    else:
+        print("Didn't see any News..")
+        
+    # Videos
+    if web_data.videos.value:
+
+        print("Videos Results#{}".format(len(web_data.videos.value)))
+
+        first_video = web_data.videos.value[0]
+        print("First Videos name: {} ".format(first_video.name))
+        print("First Videos URL: {} ".format(first_video.url))
+
+    else:
+        print("Didn't see any Videos..")
+        
