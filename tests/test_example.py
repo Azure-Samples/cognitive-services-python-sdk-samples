a0a93d53fd1999de81475f5ae747543190b4ca39
Laurent Mazuel <laurent.mazuel@gmail.com>
2017-11-20 16:45:19 -0800

    Add recordings

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,70 +1,37 @@
-import os
+import os.path
 import unittest
-from mock import patch
 
-from six.moves.urllib_parse import quote_plus
-
-from azure.common.credentials import BasicTokenAuthentication
+try:
+    from unittest import mock
+except ImportError:
+    import mock
 
 from azure_devtools.scenario_tests import (
     ReplayableTest,
-    GeneralNameReplacer,
-    SubscriptionRecordingProcessor,
-    AccessTokenReplacer,
-    patch_long_run_operation_delay,
-    RecordingProcessor,
 )
 
-from example import run_example, STORAGE_ACCOUNT_NAME
+from example import run_all_samples
 
 
 TEST_CONFIG = os.path.join(os.path.dirname(__file__), 'testsettings.cfg')
 
-DUMMY_UUID = '11111111-1111-1111-1111-111111111111'
-DUMMY_SECRET = '00000000000000000000000000000000000000000000'
-DUMMY_STORAGE_NAME = 'storagemgmtexample'
 
+class SearchExampleTest(ReplayableTest):
+    """Simple test launcher for the sample.
 
-class StorageExampleTest(ReplayableTest):
-    def __init__(self, method_name, **kwargs):
-        self.scrubber = GeneralNameReplacer()
-        super(StorageExampleTest, self).__init__(
-            method_name,
-            config_file=TEST_CONFIG,
-            recording_processors=[
-                self.scrubber,
-                SubscriptionRecordingProcessor(DUMMY_UUID),
-                AccessTokenReplacer(),
-            ],
-            replay_patches=[
-                patch_long_run_operation_delay,
-            ]
-        )
-        if self.is_live:
-            constants_to_scrub = [
-                (os.environ['AZURE_CLIENT_ID'], DUMMY_UUID),
-                (os.environ['AZURE_CLIENT_SECRET'], DUMMY_SECRET),
-                (os.environ['AZURE_TENANT_ID'], DUMMY_UUID),
-                (STORAGE_ACCOUNT_NAME, DUMMY_STORAGE_NAME)
-            ]
-            for key, replacement in constants_to_scrub:
-                self.scrubber.register_name_pair(key, replacement)
-                self.scrubber.register_name_pair(quote_plus(key), replacement)
-
-    @staticmethod
-    def fake_credentials():
-        return (
-            BasicTokenAuthentication({'access_token': 'fake_token'}),
-            DUMMY_UUID
-        )
+    Be sure to set the two environment vaiables before staring it live.
+    """
+    FILTER_HEADERS = ReplayableTest.FILTER_HEADERS + ['Ocp-Apim-Subscription-Key']
 
     def test_example(self):
-        if self.is_live:
-            run_example()
+        if self.in_recording:
+            run_all_samples()
         else:
-            with patch('example.get_credentials', StorageExampleTest.fake_credentials), \
-                 patch('example.STORAGE_ACCOUNT_NAME', DUMMY_STORAGE_NAME):
-                run_example()
+            with mock.patch.dict('os.environ',
+                {"ENTITYSEARCH_SUBSCRIPTION_KEY": "0000000000000000000000000000",
+                 "ENTITYSEARCH_SUBSCRIPTION_KEY": "0000000000000000000000000000"}):
+                 run_all_samples()
+
 
 
 if __name__ == '__main__':
