f6d722bdb9bc7e62fc05e4ef13d4e54e9b0e6164
Laurent Mazuel <laurent.mazuel@gmail.com>
2017-12-19 15:57:41 -0800

    Add SpellCheck

---
+++ samples/language/spellcheck_samples.py
@@ -0,0 +1,43 @@
+from azure.cognitiveservices.language.spellcheck import SpellCheckAPI
+from msrest.authentication import CognitiveServicesCredentials
+
+SUBSCRIPTION_KEY_ENV_NAME = "SPELLCHECK_SUBSCRIPTION_KEY"
+
+def spellcheck(subscription_key):
+    """SpellCheck.
+
+    This will do a search for misspelled query and parse the response.
+    """
+    client = SpellCheckAPI(CognitiveServicesCredentials(subscription_key))
+
+    try:
+        result = client.spell_checker("Bill Gatas", mode="proof")
+        print("Correction for Query# \"bill gatas\"")
+
+        if result.flagged_tokens:
+            first_spellcheck_result = result.flagged_tokens[0]
+
+            print("SpellCheck result count: {}".format(len(result.flagged_tokens)))
+            print("First SpellCheck token: {}".format(first_spellcheck_result.token))
+            print("First SpellCheck type: {}".format(first_spellcheck_result.type))
+            print("First SpellCheck suggestion count: {}".format(len(first_spellcheck_result.suggestions)))
+
+            if first_spellcheck_result.suggestions:
+                first_suggestion = first_spellcheck_result.suggestions[0]
+                print("First SpellCheck suggestion score: {}".format(first_suggestion.score))
+                print("First SpellCheck suggestion: {}".format(first_suggestion.suggestion))
+            else:
+                print("Couldn't get any Spell check results!")
+
+        else:
+            print("Didn't see any SpellCheck results..")
+
+    except Exception as err:
+        print("Encountered exception. {}".format(err))
+
+
+if __name__ == "__main__":
+    import sys, os.path
+    sys.path.append(os.path.abspath(os.path.join(__file__, "..", "..")))    
+    from tools import execute_samples
+    execute_samples(globals(), SUBSCRIPTION_KEY_ENV_NAME)
