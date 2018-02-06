abb404f18d81fb09642e48929f7be1ff81bdfc5b
Laurent Mazuel <laurent.mazuel@gmail.com>
2018-02-06 15:48:35 -0800

    Image Review

--- samples/vision/content_moderator_samples.py
+++ samples/vision/content_moderator_samples.py
@@ -1,4 +1,5 @@
 import os.path
+from pprint import pprint
 
 from azure.cognitiveservices.vision.contentmoderator import ContentModeratorClient
 from azure.cognitiveservices.vision.contentmoderator.models import Content
@@ -7,41 +8,46 @@ from msrest.authentication import CognitiveServicesCredentials
 SUBSCRIPTION_KEY_ENV_NAME = "CONTENTMODERATOR_SUBSCRIPTION_KEY"
 CONTENTMODERATOR_LOCATION = os.environ.get("CONTENTMODERATOR_LOCATION", "westcentralus")
 
-IMAGES_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)), "images")
-
 def image_review(subscription_key):
     """ImageReview.
 
-    This will analysis an image in a review.
+    This will review an image using workflow and job.
     """
 
     # The moderation job will use this workflow that you defined earlier.
     # See the quickstart article to learn how to setup custom workflows.
+    # https://docs.microsoft.com/azure/cognitive-services/content-moderator/review-tool-user-guide/workflows
     workflow_name = "OCR"
 
     # The name of the team to assign the job to.
     # This must be the team name you used to create your Content Moderator account. You can 
-    # retrieve your team name from the Conent Moderator web site. Your team name is the Id 
+    # retrieve your team name from the Content Moderator web site. Your team name is the Id 
     # associated with your subscription.
-    team_name = "testreview6"
+    team_name = "pysdktesting"
 
+    # An image with this text:
+    # IF WE DID ALL THE THINGS WE ARE CAPABLE OF DOING, WE WOULD LITERALLY ASTOUND OURSELVE 
+    # Be sure your workflow create a review for this (e.g. OCR contains some words).
     image_url = "https://moderatorsampleimages.blob.core.windows.net/samples/sample2.jpg"
+
+    # Where you want to receive the approval/refuse event. This is the only way to get this information.
+    call_back_endpoint = "https://requestb.in/1l64pe71"
 
     client = ContentModeratorClient(
         CONTENTMODERATOR_LOCATION+'.api.cognitive.microsoft.com',
         CognitiveServicesCredentials(subscription_key)
     )
 
-    print("Create moderation job for an image.")
+    print("Create moderation job for an image.\n")
     job_result = client.reviews.create_job(
         team_name,
         "Image",     # Possible values include: 'Image', 'Text', 'Video'
         "ContentID", # Id/Name to identify the content submitted.
         workflow_name,
         "application/json", # Possible values include: 'application/json', 'image/jpeg'
-        image_url
+        image_url,
+        call_back_endpoint=call_back_endpoint
     )
-
     job_id = job_result.job_id
 
     print("Get job status before review.")
@@ -49,9 +55,33 @@ def image_review(subscription_key):
         team_name,
         job_id,
     )
+    pprint(job_details.as_dict())    
 
-    print(job_details.as_dict())
+    input("\nPerform manual reviews on the Content Moderator Review Site, and hit enter here.")
+    job_details = client.reviews.get_job_details(
+        team_name,
+        job_id,
+    )
+    pprint(job_details.as_dict())
 
+    # Your call back endpoint should have received an event like this:
+    # {
+    #   "ReviewId": "201802i2957280730e04578aefe678ba6e5ea24",
+    #   "ModifiedOn": "2018-02-06T23:46:26.3121698Z",
+    #   "ModifiedBy": "unknown",
+    #   "CallBackType": "Review",
+    #   "ContentId": "ContentID",
+    #   "ContentType": "Image",
+    #   "Metadata": {
+    #     "hastext": "True",
+    #     "ocrtext": "IF WE DID \r\nALL \r\nTHE THINGS \r\nWE ARE \r\nCAPABLE \r\nOF DOING, \r\nWE WOULD \r\nLITERALLY \r\nASTOUND \r\nOURSELVE \r\n",
+    #     "imagename": "ContentID"
+    #   },
+    #   "ReviewerResultTags": {
+    #     "a": "True",
+    #     "r": "False"
+    #   }
+    # }    
 
 if __name__ == "__main__":
     import sys, os.path
