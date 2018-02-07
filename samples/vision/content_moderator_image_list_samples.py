7ef22252ee97ff3b63bc35579cb31d7bff9bc981
Laurent Mazuel <laurent.mazuel@gmail.com>
2018-02-07 13:22:22 -0800

    Complete image list sample

--- samples/vision/content_moderator_image_list_samples.py
+++ samples/vision/content_moderator_image_list_samples.py
@@ -1,12 +1,45 @@
 import os.path
 from pprint import pprint
+import time
 
 from azure.cognitiveservices.vision.contentmoderator import ContentModeratorClient
+from azure.cognitiveservices.vision.contentmoderator.models import (
+    APIErrorException,
+    ImageList,
+    ImageIds,
+    Image,
+    RefreshIndex,
+    MatchResponse
+)
 from msrest.authentication import CognitiveServicesCredentials
 
 SUBSCRIPTION_KEY_ENV_NAME = "CONTENTMODERATOR_SUBSCRIPTION_KEY"
 CONTENTMODERATOR_LOCATION = os.environ.get("CONTENTMODERATOR_LOCATION", "westcentralus")
 
+# The number of minutes to delay after updating the search index before
+# performing image match operations against the list.
+LATENCY_DELAY = 0.5
+
+IMAGE_LIST = {
+    "Sports": [
+        "https://moderatorsampleimages.blob.core.windows.net/samples/sample4.png",
+        "https://moderatorsampleimages.blob.core.windows.net/samples/sample6.png",
+        "https://moderatorsampleimages.blob.core.windows.net/samples/sample9.png"
+    ],
+    "Swimsuit": [
+        "https://moderatorsampleimages.blob.core.windows.net/samples/sample1.jpg",
+        "https://moderatorsampleimages.blob.core.windows.net/samples/sample3.png",
+        "https://moderatorsampleimages.blob.core.windows.net/samples/sample4.png",
+        "https://moderatorsampleimages.blob.core.windows.net/samples/sample16.png"        
+    ]
+}
+
+IMAGES_TO_MATCH = [
+    "https://moderatorsampleimages.blob.core.windows.net/samples/sample1.jpg",
+    "https://moderatorsampleimages.blob.core.windows.net/samples/sample4.png",
+    "https://moderatorsampleimages.blob.core.windows.net/samples/sample5.png",
+    "https://moderatorsampleimages.blob.core.windows.net/samples/sample16.png"    
+]
 
 def image_lists(subscription_key):
     """ImageList.
@@ -31,20 +64,153 @@ def image_lists(subscription_key):
             }
         }
     )
-    print("Item created:")
+    print("List created:")
+    assert isinstance(custom_list, ImageList)
     pprint(custom_list.as_dict())
     list_id = custom_list.id
 
+    #
+    # Add images
+    #
+
     def add_images(list_id, image_url, label):
-        print("Adding image {} to list {} with label {}.".format(image_url, list_id, label))
-        added_image = client.list_management_image.add_image_url_input(
-            list_id,
+        """Generic add_images from url and label."""
+        print("\nAdding image {} to list {} with label {}.".format(image_url, list_id, label))
+        try:
+            added_image = client.list_management_image.add_image_url_input(
+                list_id,
+                "application/json",
+                data_representation="URL",
+                value=image_url,
+                label=label
+            )
+        except APIErrorException as err:
+            # sample4 will fail
+            print("Unable to add image to list: {}".format(err))
+        else:
+            assert isinstance(added_image, Image)
+            pprint(added_image.as_dict())
+            return added_image
+
+    print("\nAdding images to list {}".format(list_id))
+    index = {}  # Keep an index url to id for later removal
+    for label, urls in IMAGE_LIST.items():
+        for url in urls:
+            image = add_images(list_id, url, label)
+            if image:
+                index[url] = image.content_id
+
+    #
+    # Get all images ids
+    #
+    print("\nGetting all image IDs for list {}".format(list_id))
+    image_ids = client.list_management_image.get_all_image_ids(list_id)
+    assert isinstance(image_ids, ImageIds)
+    pprint(image_ids.as_dict())
+
+    #
+    # Update list details
+    #
+    print("\nUpdating details for list {}".format(list_id))
+    updated_list = client.list_management_image_lists.update(
+        list_id,
+        "application/json",
+        {
+            "name": "Swimsuits and sports"
+        }
+    )
+    assert isinstance(updated_list, ImageList)
+    pprint(updated_list.as_dict())
+    
+    #
+    # Get list details
+    #
+    print("\nGetting details for list {}".format(list_id))
+    list_details = client.list_management_image_lists.get_details(list_id)
+    assert isinstance(list_details, ImageList)
+    pprint(list_details.as_dict())
+
+    #
+    # Refresh the index
+    #
+    print("\nRefreshing the search index for list {}".format(list_id))
+    refresh_index = client.list_management_image_lists.refresh_index_method(list_id)
+    assert isinstance(refresh_index, RefreshIndex)
+    pprint(refresh_index.as_dict())
+
+    print("\nWaiting {} minutes to allow the server time to propagate the index changes.".format(LATENCY_DELAY))
+    time.sleep(LATENCY_DELAY * 60)
+
+    #
+    # Match images against the image list.
+    #
+    for image_url in IMAGES_TO_MATCH:
+        print("\nMatching image {} against list {}".format(image_url, list_id))
+        match_result = client.image_moderation.match_url_input(
             "application/json",
+            list_id,
             data_representation="URL",
-            value=image_url,
-            label=label
+            value=image_url,            
         )
-        pprint(added_image.as_dict())
+        assert isinstance(match_result, MatchResponse)
+        print("Is match? {}".format(match_result.is_match))
+        print("Complete match details:")
+        pprint(match_result.as_dict())
+
+    #
+    # Remove images
+    #
+    correction = "https://moderatorsampleimages.blob.core.windows.net/samples/sample16.png"
+    print("\nRemove image {} from list {}".format(correction, list_id))
+    client.list_management_image.delete_image(
+        list_id,
+        index[correction]
+    )
+
+    #
+    # Refresh the index
+    #
+    print("\nRefreshing the search index for list {}".format(list_id))
+    client.list_management_image_lists.refresh_index_method(list_id)
+
+    print("\nWaiting {} minutes to allow the server time to propagate the index changes.".format(LATENCY_DELAY))
+    time.sleep(LATENCY_DELAY * 60)
+
+    #
+    # Re-match
+    #
+    print("\nMatching image. The removed image should not match")
+    for image_url in IMAGES_TO_MATCH:
+        print("\nMatching image {} against list {}".format(image_url, list_id))
+        match_result = client.image_moderation.match_url_input(
+            "application/json",
+            list_id,
+            data_representation="URL",
+            value=image_url,            
+        )
+        assert isinstance(match_result, MatchResponse)
+        print("Is match? {}".format(match_result.is_match))
+        print("Complete match details:")
+        pprint(match_result.as_dict())
+
+    #
+    # Delete all images
+    #
+    print("\nDelete all images in the image list {}".format(list_id))
+    client.list_management_image.delete_all_images(list_id)
+
+    #
+    # Delete list
+    #
+    print("\nDelete the image list {}".format(list_id))
+    client.list_management_image_lists.delete(list_id)
+
+    #
+    # Get all list ids
+    #
+    print("\nVerify that the list {} was deleted.".format(list_id))
+    image_lists = client.list_management_image_lists.get_all_image_lists()
+    assert not any(list_id == image_list.id for image_list in image_lists)
 
 if __name__ == "__main__":
     import sys, os.path
