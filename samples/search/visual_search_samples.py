4c545740ba6ce6aa8aa236f38e231270cf5c5dd4
Isabella Cai <iscai@microsoft.com>
2018-11-19 15:11:56 -0800

    working on visual search samples, trying out s9 key

--- samples/search/visual_search_samples.py
+++ samples/search/visual_search_samples.py
@@ -198,5 +198,5 @@ def search_insights_token_with_crop_area(subscription_key):
 if __name__ == "__main__":
     import sys, os.path
     sys.path.append(os.path.abspath(os.path.join(__file__, "..", "..")))
-    from tools import execute_samples
+    from samples.tools import execute_samples
     execute_samples(globals(), SUBSCRIPTION_KEY_ENV_NAME)
