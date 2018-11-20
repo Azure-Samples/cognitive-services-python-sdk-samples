7a23c666df32b04eff5359e9d3f931bbe07c1798
Isabella Cai <iscai@microsoft.com>
2018-11-20 12:55:57 -0800

    added .idea to gitignore and edited import for tools

--- samples/search/custom_search_samples.py
+++ samples/search/custom_search_samples.py
@@ -28,5 +28,5 @@ def custom_search_web_page_result_lookup(subscription_key):
 if __name__ == "__main__":
     import sys, os.path
     sys.path.append(os.path.abspath(os.path.join(__file__, "..", "..")))
-    from samples.tools import execute_samples
+    from tools import execute_samples
     execute_samples(globals(), SUBSCRIPTION_KEY_ENV_NAME)
