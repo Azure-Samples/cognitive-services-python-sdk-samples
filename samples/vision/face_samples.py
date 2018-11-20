b9dd93bb9cbac3487b9a3c3915d0f2dfde49ef3f
Isabella Cai <iscai@microsoft.com>
2018-11-19 16:31:13 -0800

    changed samples.tools import to tools import

--- samples/vision/face_samples.py
+++ samples/vision/face_samples.py
@@ -693,5 +693,5 @@ def verify_in_large_person_group(subscription_key):
 if __name__ == "__main__":
     import sys, os.path
     sys.path.append(os.path.abspath(os.path.join(__file__, "..", "..")))
-    from samples.tools import execute_samples
+    from tools import execute_samples
     execute_samples(globals(), SUBSCRIPTION_KEY_ENV_NAME)
