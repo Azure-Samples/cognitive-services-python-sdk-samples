00fe19fd0e8e133fdc1603839e776ecf5c858f16
Isabella Cai <iscai@microsoft.com>
2018-11-19 16:53:56 -0800

    added visual search samples

--- samples/search/visual_search_samples.py
+++ samples/search/visual_search_samples.py
@@ -195,8 +195,65 @@ def search_insights_token_with_crop_area(subscription_key):
     else:
         print("Couldn't find image tags!")
 
+
+def search_url_with_json(subscription_key):
+    client = VisualSearchAPI(CognitiveServicesCredentials(subscription_key))
+    try:
+        """
+         The visual search request can be passed in as a JSON string
+         The image is specified via URL in the ImageInfo object, along with a crop area as shown below:
+         {
+           "imageInfo": {
+             "url": "https://images.unsplash.com/photo-1512546148165-e50d714a565a?w=600&q=80",
+             "cropArea": {
+               "top": 0.1,
+               "bottom": 0.5,
+               "left": 0.1,
+               "right": 0.9
+             }
+           },
+           "knowledgeRequest": {
+             "filters": {
+               "site": "www.bing.com"
+             }
+           }
+         } 
+        """
+        visual_search_request_json = "{\"imageInfo\":{\"url\":\"https://images.unsplash.com/photo-1512546148165-e50d714a565a?w=600&q=80\",\"cropArea\":{\"top\":0.1,\"bottom\":0.5,\"left\":0.1,\"right\":0.9}},\"knowledgeRequest\":{\"filters\":{\"site\":\"www.bing.com\"}}}";
+
+        # An image binary is not necessary here, as the image is specified via URL
+        visual_search_results = client.images.visual_search(knowledge_request=visual_search_request_json)
+        print("Search visual search request with url of dog image")
+
+        if visual_search_results is None:
+            print("No visual search result data.")
+        else:
+            # Visual Search results
+            if visual_search_results.image.image_insights_token:
+                print("Uploaded image insights token: {}".format(visual_search_results.image.image_insights_token))
+            else:
+                print("Couldn't find image insights token!")
+
+            # List of tags
+            if len(visual_search_results.tags) > 0:
+                first_tag_result = visual_search_results.tags[0]
+                print("Visual search tag count: {}".format(len(visual_search_results.tags)))
+
+                # List of actions in first tag
+                if len(first_tag_result.actions) > 0:
+                    first_action_result = first_tag_result.actions[0]
+                    print("First tag action count: {}".format(len(first_tag_result.actions)))
+                    print("First tag action type: {}".format(first_action_result.action_type))
+                else:
+                    print("Couldn't find tag actions!")
+            else:
+                print("Couldn't find image tags!")
+
+    except Exception as e:
+        print("Encountered exception. " + str(e))
+
 if __name__ == "__main__":
     import sys, os.path
     sys.path.append(os.path.abspath(os.path.join(__file__, "..", "..")))
-    from samples.tools import execute_samples
+    from tools import execute_samples
     execute_samples(globals(), SUBSCRIPTION_KEY_ENV_NAME)
