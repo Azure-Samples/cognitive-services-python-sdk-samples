8e8b52cede35f33a5666a097f3fa09b03b73d651
Isabella Cai <iscai@microsoft.com>
2018-11-27 14:55:39 -0800

    about to test whether style changes break the sample

--- samples/search/custom_image_search_samples.py
+++ samples/search/custom_image_search_samples.py
@@ -5,21 +5,22 @@ SUBSCRIPTION_KEY_ENV_NAME = "CUSTOMIMAGESEARCH_SUBSCRIPTION_KEY"
 
 
 def custom_image_search_result_lookup(subscription_key):
-    """CustomImageSearchResultLookup
+    """CustomImageSearchResultLookup.
 
     This will look up a single query (Xbox) and print out number of results, insights token, thumbnail url, content url for the first image result
     """
+
     client = CustomImageSearchAPI(credentials=CognitiveServicesCredentials(subscription_key))
     try:
         image_results = client.custom_instance.image_search(query="Xbox", custom_config=1)
         print("Searched for Query \" Xbox \"")
 
         # WebPages
-        if (len(image_results.value) > 0):
+        if image_results.value:
             # find the first web page
             first_image_result = image_results.value[0]
 
-            if (first_image_result):
+            if first_image_result:
                 print("Image result count: {}".format(len(image_results.value)))
                 print("First image insights token: {}".format(first_image_result.image_insights_token))
                 print("First image thumbnail url: {}".format(first_image_result.thumbnail_url))
@@ -30,8 +31,6 @@ def custom_image_search_result_lookup(subscription_key):
             print("Couldn't find image results!")
     except Exception as e:
         print("encountered exception. " + str(e))
-
-
 
 if __name__ == "__main__":
     import sys, os.path
