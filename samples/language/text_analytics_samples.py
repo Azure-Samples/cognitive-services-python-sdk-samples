291f0bbb040cfaabedc78928500f507166df15ca
Isabella Cai <iscai@microsoft.com>
2018-12-21 15:06:07 -0800

    added entity extraction samples, untested because blocked swagger side

--- samples/language/text_analytics_samples.py
+++ samples/language/text_analytics_samples.py
@@ -2,18 +2,20 @@
 
 import os
 
-from azure.cognitiveservices.language.textanalytics import TextAnalyticsAPI
+from azure.cognitiveservices.language.textanalytics import TextAnalyticsClient
 from msrest.authentication import CognitiveServicesCredentials
 
 SUBSCRIPTION_KEY_ENV_NAME = "TEXTANALYTICS_SUBSCRIPTION_KEY"
 TEXTANALYTICS_LOCATION = os.environ.get("TEXTANALYTICS_LOCATION", "westcentralus")
+
 
 def language_extraction(subscription_key):
     """Language extraction.
 
     This will detect the language of a few strings.
     """
-    client = TextAnalyticsAPI(TEXTANALYTICS_LOCATION, CognitiveServicesCredentials(subscription_key))
+    endpoint = "https://{}.api.cognitive.microsoft.com".format(TEXTANALYTICS_LOCATION)
+    client = TextAnalyticsClient(endpoint=endpoint, credentials=CognitiveServicesCredentials(subscription_key))
 
     try:
         documents = [{
@@ -38,12 +40,14 @@ def language_extraction(subscription_key):
     except Exception as err:
         print("Encountered exception. {}".format(err))
 
+
 def key_phrases(subscription_key):
     """Key-phrases.
 
     The API returns a list of strings denoting the key talking points in the input text.
     """
-    client = TextAnalyticsAPI(TEXTANALYTICS_LOCATION, CognitiveServicesCredentials(subscription_key))
+    endpoint = "https://{}.api.cognitive.microsoft.com".format(TEXTANALYTICS_LOCATION)
+    client = TextAnalyticsClient(endpoint=endpoint, credentials=CognitiveServicesCredentials(subscription_key))
 
     try:
         documents = [{
@@ -85,7 +89,8 @@ def sentiment(subscription_key):
 
     Scores close to 1 indicate positive sentiment, while scores close to 0 indicate negative sentiment.
     """
-    client = TextAnalyticsAPI(TEXTANALYTICS_LOCATION, CognitiveServicesCredentials(subscription_key))
+    endpoint = "https://{}.api.cognitive.microsoft.com".format(TEXTANALYTICS_LOCATION)
+    client = TextAnalyticsClient(endpoint=endpoint, credentials=CognitiveServicesCredentials(subscription_key))
 
     try:
         documents = [{
@@ -120,8 +125,59 @@ def sentiment(subscription_key):
         print("Encountered exception. {}".format(err))
 
 
+def entity_extraction(subscription_key):
+    """EntityExtraction.
+
+    Extracts the entities from sentences and prints out their properties
+    """
+    endpoint = "https://{}.api.cognitive.microsoft.com".format(TEXTANALYTICS_LOCATION)
+    client = TextAnalyticsClient(endpoint=endpoint, credentials=CognitiveServicesCredentials(subscription_key))
+
+    try:
+        documents = [{
+            'language': 'en',
+            'id': 0,
+            'text': "Microsoft released win10. Microsoft also released Hololens"
+        }, {
+            'language': 'en',
+            'id': 1,
+            'text': "Microsoft is an IT company."
+        }, {
+            'language': 'es',
+            'id': 2,
+            'text': "Microsoft lanzó win10. Microsoft también lanzó Hololens"
+        }, {
+            'language': 'es',
+            'id': 3,
+            'text': "Microsoft es una empresa de TI."
+        }]
+        for document in documents:
+            print("Extracting entities from '{}' (id: {})".format(document['text'], document['id']))
+
+        response = client.entities(
+            documents=documents
+        )
+
+        for document in response.documents:
+            print("Document ID: {}".format(document['Id']))
+            print("\t Entities:")
+            for entity in document['Entities']:
+                print("\t\tEntity Name: {}".format(entity.name))
+                print("\t\tWikipedia Language: {}".format(entity.wikipedia_language))
+                print("\t\tWikipedia Url: {}".format(entity.wikipedia_url))
+                print("\t\tNumber of times appeared on the text: {}".format(len(entity.matches)))
+                print("\t\tEntity Type: {}".format(entity.type))
+                print("\t\tEntity SubType: {}".format(entity.sub_type))
+                print("\n")
+
+    except Exception as err:
+        print("Encountered exception. {}".format(err))
+
+
 if __name__ == "__main__":
     import sys, os.path
-    sys.path.append(os.path.abspath(os.path.join(__file__, "..", "..")))    
+
+    sys.path.append(os.path.abspath(os.path.join(__file__, "..", "..")))
     from tools import execute_samples
+
     execute_samples(globals(), SUBSCRIPTION_KEY_ENV_NAME)
