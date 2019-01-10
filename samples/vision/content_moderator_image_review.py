b15aad87a307e38ed70e7f6149cf480038a7bfa2
Isabella Cai <iscai@microsoft.com>
2019-01-10 14:10:42 -0800

    fixed image review sample

--- samples/vision/content_moderator_image_review.py
+++ samples/vision/content_moderator_image_review.py
@@ -3,7 +3,6 @@ from pprint import pprint
 import uuid
 
 from azure.cognitiveservices.vision.contentmoderator import ContentModeratorClient
-from azure.cognitiveservices.vision.contentmoderator.models import Content, Review
 from msrest.authentication import CognitiveServicesCredentials
 
 SUBSCRIPTION_KEY_ENV_NAME = "CONTENTMODERATOR_SUBSCRIPTION_KEY"
@@ -19,7 +18,7 @@ def image_review(subscription_key):
     # This must be the team name you used to create your Content Moderator account. You can 
     # retrieve your team name from the Content Moderator web site. Your team name is the Id 
     # associated with your subscription.
-    team_name = "pysdktesting"
+    team_name = "cspythonsdk"
 
     # An image to review
     image_url = "https://moderatorsampleimages.blob.core.windows.net/samples/sample5.png"
@@ -28,8 +27,8 @@ def image_review(subscription_key):
     call_back_endpoint = "https://requestb.in/qmsakwqm"
 
     client = ContentModeratorClient(
-        CONTENTMODERATOR_LOCATION+'.api.cognitive.microsoft.com',
-        CognitiveServicesCredentials(subscription_key)
+        endpoint='https://'+CONTENTMODERATOR_LOCATION+'.api.cognitive.microsoft.com',
+        credentials=CognitiveServicesCredentials(subscription_key)
     )
 
     print("Create review for {}.\n".format(image_url))
@@ -45,9 +44,9 @@ def image_review(subscription_key):
     }
 
     reviews = client.reviews.create_reviews(
-        "application/json",
-        team_name,
-        [review_item]  # As many review item as you need
+        url_content_type="application/json",
+        team_name=team_name,
+        create_review_body=[review_item]  # As many review item as you need
     )
     review_id = reviews[0]  # Ordered list of string of review ID
 
@@ -62,21 +61,17 @@ def image_review(subscription_key):
     pprint(review_details.as_dict())
 
     # Your call back endpoint should have received an event like this:
-    # {
-    #   "ReviewId": "201802idca99b6f3f7d4418b381d8c1bcc7e99a",
-    #   "ModifiedOn": "2018-02-07T22:39:17.2868098Z",
-    #   "ModifiedBy": "unknown",
-    #   "CallBackType": "Review",
-    #   "ContentId": "3dd26599-264a-4c3d-af00-3d8726a59e95",
-    #   "ContentType": "Image",
-    #   "Metadata": {
-    #     "sc": "True"
-    #   },
-    #   "ReviewerResultTags": {
-    #     "a": "True",
-    #     "r": "False"
-    #   }
-    # }
+    # {'callback_endpoint': 'https://requestb.in/qmsakwqm',
+    #  'content': '',
+    #  'content_id': '3ebe16cb-31ed-4292-8b71-1dfe9b0e821f',
+    #  'created_by': 'cspythonsdk',
+    #  'metadata': [{'key': 'sc', 'value': 'True'}],
+    #  'review_id': '201901i14682e2afe624fee95ebb248643139e7',
+    #  'reviewer_result_tags': [{'key': 'a', 'value': 'True'},
+    #                           {'key': 'r', 'value': 'True'}],
+    #  'status': 'Complete',
+    #  'sub_team': 'public',
+    #  'type': 'Image'}
 
 if __name__ == "__main__":
     import sys, os.path
