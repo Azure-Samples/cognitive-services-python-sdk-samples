dfb7755df22d3e1499ad05685270f869cea3eadc
Winona Azure <v-wiazur@microsoft.com>
2019-04-29 17:37:38 -0700

    Person Group sample initial commit, plus adding images

---
+++ samples/vision/face_person_group_samples.py
@@ -0,0 +1,101 @@
+import os, io, uuid, glob
+from msrest.authentication import CognitiveServicesCredentials
+from azure.cognitiveservices.vision.face import FaceClient
+from azure.cognitiveservices.vision.face.models import TrainingStatusType, Person
+
+'''
+PersonGroup - Face API sample
+References: 
+    How-to guide: https://docs.microsoft.com/en-us/azure/cognitive-services/face/face-api-how-to-topics/howtoidentifyfacesinimage
+    SDK: https://docs.microsoft.com/en-us/python/api/azure-cognitiveservices-vision-face/azure.cognitiveservices.vision.face?view=azure-python
+    Sample images to download: https://github.com/Microsoft/Cognitive-Face-Windows/tree/master/Data
+Prerequisites:
+    Python 3+
+    Install Face SDK: pip install azure-cognitiveservices-vision-face
+'''
+# Group image for testing against
+group_photo = 'test-image.jpg'
+
+IMAGES_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)), "images", "Face")
+
+''' 
+Authentication
+'''
+# Replace with a valid subscription key (keeping the quotes in place).
+KEY = 'FACE_SUBSCRIPTION_KEY'
+# Replace westus if it's not your region
+BASE_URL = 'https://westus.api.cognitive.microsoft.com'
+face_client = FaceClient(BASE_URL, CognitiveServicesCredentials(KEY))
+
+''' 
+Create the PersonGroup
+'''
+# Create empty person group
+person_group_id = str(uuid.uuid4()) # generate random ID
+face_client.person_group.create(person_group_id=person_group_id, name=person_group_id)
+
+# Define woman friend 
+woman = face_client.person_group_person.create(person_group_id, "Woman")
+# Define man friend
+man = face_client.person_group_person.create(person_group_id, "Man")
+# Define child friend
+child = face_client.person_group_person.create(person_group_id, "Child")
+
+'''
+Detect faces and register to correct person
+'''
+# Find all jpeg images of friends in working directory
+woman_images = [file for file in glob.glob('*.jpg') if file.startswith("woman")]
+man_images = [file for file in glob.glob('*.jpg') if file.startswith("man")]
+child_images = [file for file in glob.glob('*.jpg') if file.startswith("child")]
+
+# Add to a woman person
+for image in woman_images:
+    w = open(image, 'r+b')
+    face_client.person_group_person.add_face_from_stream(person_group_id, woman.person_id, w)
+
+# Add to a man person
+for image in man_images:
+    m = open(image, 'r+b')
+    face_client.person_group_person.add_face_from_stream(person_group_id, man.person_id, m)
+
+# Add to a child person
+for image in child_images:
+    ch = open(image, 'r+b')
+    face_client.person_group_person.add_face_from_stream(person_group_id, child.person_id, ch)
+
+''' 
+Train PersonGroup
+'''
+# Train the person group
+face_client.person_group.train(person_group_id)
+training_status = face_client.person_group.get_training_status(person_group_id)
+if training_status.status == TrainingStatusType.failed:
+    raise Exception('Training failed with message {}.'.format(training_status.message))
+
+
+'''
+Identify a face against a defined PersonGroup
+'''
+# Get test image
+test_image_array = glob.glob(os.path.join(IMAGES_FOLDER, group_photo))
+ti = open(test_image_array[0], 'r+b')
+
+# Detect faces
+face_ids = []
+faces = face_client.face.detect_with_stream(ti)
+for face in faces:
+    face_ids.append(face.face_id)
+
+# Identify faces
+results = face_client.face.identify(face_ids, person_group_id)
+if not results:
+    print('No person identified in the person group for faces from the {}.'.format(ti.name))
+for person in results:
+    person_identified = face_client.person_group_person.get(person_group_id, person.candidates[0].person_id)
+    print('Person {}\'s face is identified in {}: person ID is {}, confidence is {}.'.format(person_identified.name,
+    ti.name,
+    person.face_id,
+    person.candidates[0].confidence))
+
+
