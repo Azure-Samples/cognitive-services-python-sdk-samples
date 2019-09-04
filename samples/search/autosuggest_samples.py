d16454e7cede7ce0eec0f87bca58bf64ca0ed0b5
Winona Azure <38537084+wiazur@users.noreply.github.com>
2019-09-03 20:40:34 -0700

    Updated key
    The authentication information is moving to a standard of getting it from environment variables.

--- samples/search/autosuggest_samples.py
+++ samples/search/autosuggest_samples.py
@@ -7,7 +7,8 @@ from azure.cognitiveservices.search.autosuggest.models import (
 )
 from msrest.authentication import CognitiveServicesCredentials
 
-SUBSCRIPTION_KEY_ENV_NAME = "AUTOSUGGEST_SUBSCRIPTION_KEY"
+# Add your Bing Autosuggest subscription key to your environment variables.
+SUBSCRIPTION_KEY= os.environ['BING_AUTOSUGGEST_SUBSCRIPTION_KEY']
 
 
 def autosuggest_lookup(subscription_key):
@@ -73,4 +74,4 @@ if __name__ == "__main__":
     import os.path
     sys.path.append(os.path.abspath(os.path.join(__file__, "..", "..")))
     from tools import execute_samples
-    execute_samples(globals(), SUBSCRIPTION_KEY_ENV_NAME)
+    execute_samples(globals(), SUBSCRIPTION_KEY)
