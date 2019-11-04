43ddef95f1c8a67437292acae66192946e551890
Aaron Hill <aahil@microsoft.com>
2019-11-04 14:13:03 -0800

    updating python code sample

--- samples/search/custom_search_samples.py
+++ samples/search/custom_search_samples.py
@@ -1,20 +1,27 @@
+# <imports>
+import os
 from azure.cognitiveservices.search.customsearch import CustomSearchClient
 from msrest.authentication import CognitiveServicesCredentials
+# </imports>
 
+# <vars>
 SUBSCRIPTION_KEY = os.environ['BING_CUSTOM_SEARCH_SUBSCRIPTION_KEY']
+search_term = "xbox" 
+custom_config = "your-custom-config-id" # You can also use "1"
+# </vars>
 
+# <authentication>
+client = CustomSearchClient(CognitiveServicesCredentials(SUBSCRIPTION_KEY))
+# </authentication>
 
-def custom_search_web_page_result_lookup(subscription_key):
-    """CustomSearch.
-
-    This will look up a single query (Xbox) and print out name and url for first web result.
-    """
-
-    client = CustomSearchClient(CognitiveServicesCredentials(subscription_key))
-
+"""CustomSearch.
+This will look up a single query and print out name and url for first web result.
+"""
+# <request>
+def custom_search_web_page_result_lookup():
     try:
-        web_data = client.custom_instance.search(query="xbox", custom_config=1)
-        print("Searched for Query 'xbox'")
+        web_data = client.custom_instance.search(query=search_term, custom_config=1)
+        print("Searched for Query: " + search_term)
 
         if web_data.web_pages.value:
             first_web_result = web_data.web_pages.value[0]
@@ -27,7 +34,7 @@ def custom_search_web_page_result_lookup(subscription_key):
 
     except Exception as err:
         print("Encountered exception. {}".format(err))
-
+# <request>
 
 if __name__ == "__main__":
     import sys
