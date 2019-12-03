72b4301e5f5289bb6d630e65bc5514ee1fbb08f6
Winona Azure <38537084+wiazur@users.noreply.github.com>
2019-12-02 20:22:12 -0800

    Matched variables

--- samples/search/news_search_samples.py
+++ samples/search/news_search_samples.py
@@ -149,4 +149,4 @@ if __name__ == "__main__":
     import sys, os.path
     sys.path.append(os.path.abspath(os.path.join(__file__, "..", "..", "..")))    
     from samples.tools import execute_samples
-    execute_samples(globals(), SUBSCRIPTION_KEY_ENV_NAME)
+    execute_samples(globals(), SUBSCRIPTION_KEY)
