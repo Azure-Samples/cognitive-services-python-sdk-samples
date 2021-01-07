982e5d0c2ffa6e95a92536b43fcfd5effd2f2c5a
Tong Xu (Wicresoft North America Ltd) <v-xuto@microsoft.com>
2021-01-07 15:05:28 +0800

    Update anomaly detector from track1 to track2

--- samples/anomaly_detector_samples.py
+++ samples/anomaly_detector_samples.py
@@ -1,13 +1,13 @@
-from azure.cognitiveservices.anomalydetector import AnomalyDetectorClient
-from azure.cognitiveservices.anomalydetector.models import Request, Point, Granularity, \
-    APIErrorException
-from datetime import datetime, timezone
-from msrest.authentication import CognitiveServicesCredentials
+from azure.ai.anomalydetector import AnomalyDetectorClient
+from azure.ai.anomalydetector.models import DetectRequest, TimeSeriesPoint, TimeGranularity, \
+    AnomalyDetectorError
+from azure.core.credentials import AzureKeyCredential
 import pandas as pd
 import os
 
+
 # Add your Azure Anomaly Detector subscription key to your environment variables.
-SUBSCRIPTION_KEY = os.environ.get["ANOMALY_DETECTOR_SUBSCRIPTION_KEY"]
+SUBSCRIPTION_KEY = os.environ["ANOMALY_DETECTOR_SUBSCRIPTION_KEY"]
 
 CSV_FOLDER = os.path.join(os.path.dirname(
     os.path.realpath(__file__)), "csv_files")
@@ -17,61 +17,59 @@ def get_series_from_file(path):
     df = pd.read_csv(path, header=None, encoding="utf-8", parse_dates=[0])
     series = []
     for index, row in df.iterrows():
-        series.append(Point(timestamp=row[0], value=row[1]))
+        series.append(TimeSeriesPoint(timestamp=row[0], value=row[1]))
     return series
 
 
 def get_request():
     series = get_series_from_file(os.path.join(
         CSV_FOLDER, "anomaly_detector_daily_series.csv"))
-    return Request(series=series, granularity=Granularity.daily)
+    return DetectRequest(series=series, granularity=TimeGranularity.daily)
 
 
 def entire_detect(subscription_key):
     print("Sample of detecting anomalies in the entire series.")
     # Add your Azure Anomaly Detector subscription key to your environment variables.
-    endpoint = os.environ.get["ANOMALY_DETECTOR_ENDPOINT"]
+    endpoint = os.environ["ANOMALY_DETECTOR_ENDPOINT"]
     
+    client = AnomalyDetectorClient(AzureKeyCredential(subscription_key), endpoint)
+    request = get_request()
+
     try:
-        client = AnomalyDetectorClient(
-            endpoint, CognitiveServicesCredentials(subscription_key))
-        request = get_request()
-        response = client.entire_detect(request)
-        if True in response.is_anomaly:
-            print("Anomaly was detected from the series at index:")
-            for i in range(len(request.series)):
-                if response.is_anomaly[i]:
-                    print(i)
-        else:
-            print("There is no anomaly detected from the series.")
+        response = client.detect_entire_series(request)
+    except AnomalyDetectorError as e:
+        print('Error code: {}'.format(e.error.code), 'Error message: {}'.format(e.error.message))
     except Exception as e:
-        if isinstance(e, APIErrorException):
-            print("Error code: {}".format(e.error.code))
-            print("Error message: {}".format(e.error.message))
-        else:
-            print(e)
+        print(e)
+
+    if any(response.is_anomaly):
+        print('Anomaly was detected from the series at index:')
+        for i, value in enumerate(response.is_anomaly):
+            if value:
+                print(i)
+    else:
+        print('No anomalies were detected in the time series.')
 
 
 def last_detect(subscription_key):
     print("Sample of detecting whether the latest point in series is anomaly.")
     # Add your Azure Anomaly Detector subscription key to your environment variables.
-    endpoint = os.environ.get["ANOMALY_DETECTOR_ENDPOINT"]
+    endpoint = os.environ["ANOMALY_DETECTOR_ENDPOINT"]
     
+    client = AnomalyDetectorClient(AzureKeyCredential(subscription_key), endpoint)
+    request = get_request()
+
     try:
-        client = AnomalyDetectorClient(
-            endpoint, CognitiveServicesCredentials(subscription_key))
-        request = get_request()
-        response = client.last_detect(request)
-        if response.is_anomaly:
-            print("The latest point is detected as anomaly.")
-        else:
-            print("The latest point is not detected as anomaly.")
+        response = client.detect_last_point(request)
+    except AnomalyDetectorError as e:
+        print('Error code: {}'.format(e.error.code), 'Error message: {}'.format(e.error.message))
     except Exception as e:
-        if isinstance(e, APIErrorException):
-            print("Error code: {}".format(e.error.code))
-            print("Error message: {}".format(e.error.message))
-        else:
-            print(e)
+        print(e)
+
+    if response.is_anomaly:
+        print('The latest point is detected as anomaly.')
+    else:
+        print('The latest point is not detected as anomaly.')
 
 
 if __name__ == "__main__":
